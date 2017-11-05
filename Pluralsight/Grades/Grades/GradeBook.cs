f4411c7f99178725495c7d3d1e6a5784c25a5d89
Bret <positivecharge8@gmail.com>
2017-11-04 21:14:28 -0700

    C# Fundamentals: Chapter 3 Classes and Objects

---
+++ Pluralsight/Grades/Grades/GradeBook.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        // ctor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades) {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            // try catch was not originally part of this lesson
+            // but put it in there anywhere in case grades.Count = 0;
+            try {
+                stats.AverageGrade = sum / grades.Count;
+            } catch (Exception e) {
+                Console.WriteLine("Invalid except: {0}", e);
+            }
+
+            return stats;
+        }
+
+        // class members broadly fall into two classes:
+        // 1. state
+        // 2. behavior
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        // <> is generic type syntax
+        private List<float> grades;
+
+        // if undeclared, private is default access modifier
+
+        // access modifieers = public or private
+
+
+
+        // STATIC
+        // use static members of a class w/out creating an instance
+        // static field or method is accessed without creating an instance
+
+        // this is how Console.WriteLine works
+        // console is a class but you dont need an instance of
+        // console to use that class
+        // aka dont have to write console x = new console();
+    }
+}
---
+++ Pluralsight/Grades/Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //behind the scenes a constructor method is being invoked so () required
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            // cw tab tab for console.writeLine();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            // value inside of book is copied and placed inside book 2 (commnented out below)
+            // by value we mean pointer
+            // GradeBook book2 = book;
+            // the add commented out below will mean book will also get it
+            // because the memory is what is being manipulated
+            // book2.AddGrade(75);
+
+            // Classes are reference types
+            // Variables hold a pointer value
+
+            // sometimes we talk about encapsulation as hiding something
+            // like "lets encapsulate the data to the class"
+        }
+    }
+}
