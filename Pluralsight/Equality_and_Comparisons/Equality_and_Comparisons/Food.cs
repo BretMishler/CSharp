bc5b626cbe4902265a7b688e13fcc302c5ae815e
Bret <positivecharge8@gmail.com>
2017-12-27 18:41:40 -0800

    Equality and Comparisons: Chapter 2 Equality in .NET
    the Virtual Object.Equals() method

---
+++ Pluralsight/Equality_and_Comparisons/Equality_and_Comparisons/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Equality_and_Comparisons
+{
+    public class Food
+    {
+        private string _name;
+
+        public string Name { get { return _name; }}
+
+        public Food(string name)
+        {
+            this._name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+
+
+    }
+}
--- Pluralsight/Equality_and_Comparisons/Equality_and_Comparisons/Program.cs
+++ Pluralsight/Equality_and_Comparisons/Equality_and_Comparisons/Program.cs
@@ -4,9 +4,17 @@ namespace Equality_and_Comparisons
 {
     class Program
     {
+        // == is the C# equality operator, not provided by .NET
+        // .NET has no concept of operators
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
         }
     }
 }
