458e6a562c607f4e305446747658c69819075d5c
Bret <positivecharge8@gmail.com>
2017-12-28 10:55:21 -0800

    Equality and Comparisons: Chapter 2 Equality in .NET
    IEquatable<T>, String and Reference Types

--- Pluralsight/Equality_and_Comparisons/Equality_and_Comparisons/Program.cs
+++ Pluralsight/Equality_and_Comparisons/Equality_and_Comparisons/Program.cs
@@ -40,6 +40,31 @@ namespace Equality_and_Comparisons
         // IEquatable<T>
         // can be exposed to any type that wants to provide a strongly-typed
         // Equals. Which means for value types , no boxing!
+        // EXTREMELY USEFUL FOR VALUE TYPES
+        // Not so useful for reference types
+
+        // Make an int x = whatever number.
+        // if you do 
+        // x.Equals(), note two Equals method
+        // 1. takes an Object (which is the overriden object.Equals method)
+        // 2. takes an int as a param (which is Int implementation of
+        // IEquatqable<int>)
+        // because in both, we pass an int, so the compiler will see the 
+        // strongly-typed IEquatable<int> method has the best signature match
+        // 
+
+        // GOOD PRACTICE FOR OVERRIDE EQUALS
+        // if you want to implement the IEquatable<T> interface, then you
+        // should override object.Equals to do exactly the same thing as your
+        // interface method does
+
+        // NOTES
+        // Strings, delegates, and tuples override object.Equals() in order to
+        // provide value equality
+        // Object also provides a static equals method for either param to be
+        // null
+        // iEquatable<T> to provide a strongly-typed Equals method that avoids
+        // boxing for value types
 
         static void Main(string[] args)
         {
@@ -49,6 +74,10 @@ namespace Equality_and_Comparisons
 
             Console.WriteLine(three.Equals(threeAgain));
             Console.WriteLine(three.Equals(four));
+
+            // if we dont do string.Equals(object)string, compiler would have
+            // picked strongly typed IEquatable<string>, aka
+            // string's implementation of IEquatable<T>
         }
     }
 }
