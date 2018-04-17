291e650916f88edfe1749907d2f550cc46a307c2
Bret <positivecharge8@gmail.com>
2018-04-17 16:16:07 -0700

    Peterson's solution example

---
+++ RandomStuff/RandomStuff/Peterson.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// exercising Peterson's Solution for solving
+// the critical section problem
+
+public static class Peterson
+{
+    public static bool[] myFlags { get; set; }
+
+    public static char turn;
+
+    public static void Program()
+    {
+        myFlags = new bool[2];
+        turn = new char();
+
+        // these have to be true
+        myFlags[0] = true;
+        myFlags[1] = true;
+
+        Task t1 = Task.Factory.StartNew(I);
+        Task t2 = Task.Factory.StartNew(J);
+
+        Task.WaitAll(t1, t2);
+    }
+
+    static void I()
+    {
+        Console.WriteLine("In I");
+        int x = 0;
+
+        do
+        {
+            myFlags[0] = true;
+            turn = 'j';
+            Console.WriteLine("I: About to hit while loop");
+            while (myFlags[1] && turn == 'j') { };
+
+            //critical section
+            Console.WriteLine("I: critical section");
+
+            myFlags[0] = false;
+
+            //remainder section
+            x++;
+        } while (x < 5);
+
+    }
+
+    static void J()
+    {
+        Console.WriteLine("In J");
+        int x = 0;
+
+        do
+        {
+            myFlags[1] = true;
+            turn = 'i';
+            Console.WriteLine("J: About to hit while loop.");
+            while (myFlags[0] && turn == 'i') { };
+            //critical section
+            Console.WriteLine("J: critical section");
+
+            myFlags[1] = false;
+
+            //remainder section
+            x++;
+        } while (x < 5);
+    }
+}
---
+++ RandomStuff/RandomStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peterson.Program();
+        }
+    }
+}
