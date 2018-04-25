bf819d1384db78a354b6c2759d7fc98c3a44f458
Bret <positivecharge8@gmail.com>
2018-04-25 10:16:38 -0700

    Added projects

---
+++ Pluralsight/Getting_Started_with_Asynchronous_Programming/Getting_Started_with_Asynchronous_Programming/ReliableWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace ReliableWPFApplication
+{
+    public partial class MainWindow : Window
+    {
+        private int count = 1;
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+
+        private void RssButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            // sync call that blocks application
+            //var data = client.DownloadString("http://www.filipekberg.se/rss/");
+
+            // use event based async patter
+            // once string is downloaded, web client will notify us by raising an event
+            client.DownloadStringAsync(new Uri("http://www.filipekberg.se/rss/"));
+
+            client.DownloadStringCompleted += Client_DownloadStringCompleted;
+        }
+
+        private void Client_DownloadStringCompleted(object sender, 
+            DownloadStringCompletedEventArgs e)
+        {
+            // sets the rss text to the result we got from the asynch operation
+            RssText.Text = e.Result;
+
+            BusyIndicator.Visibility = Visibility.Hidden;
+
+            RssButton.IsEnabled = true;
+        }
+
+        private void CounterButton_Click(object sender, RoutedEventArgs e)
+        {
+            CounterText.Text = $"Counter: {count++}";
+        }
+    }
+}
---
+++ Pluralsight/Getting_Started_with_Asynchronous_Programming/Getting_Started_with_Asynchronous_Programming/UnreliableWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace UnreliableWPFApplication
+{
+    public partial class MainWindow : Window
+    {
+        private int count = 1;
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+        
+        // sets up eweb client, though new standard is httpclient
+        private void RssButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            var data = client.DownloadString("http://www.filipekberg.se/rss/");
+
+            // simulate slow network
+            Thread.Sleep(10000);
+
+            RssText.Text = data;
+        }
+
+        private void CounterButton_Click(object sender, RoutedEventArgs e)
+        {
+            CounterText.Text = $"Counter: {count++}";
+        }
+    }
+}
