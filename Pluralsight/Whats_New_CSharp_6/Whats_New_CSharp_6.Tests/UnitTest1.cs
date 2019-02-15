2ee72a175ba52ce0cfae83b7788071f2703e1ea4
Bret Mishler <positivecharge8@gmail.com>
2019-02-14 18:17:02 -0800

    Auto-Property Initializers

---
+++ Pluralsight/Whats_New_CSharp_6/Whats_New_CSharp_6.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Whats_New_CSharp_6.Tests
+{
+    [TestClass]
+    public class User_Tests
+    {
+        [TestMethod]
+        public void User_Creates_An_Id()
+        {
+            var u1 = new User();
+
+            Assert.AreNotEqual(Guid.Empty, u1.Id)
+        }
+    }
+}
--- Pluralsight/Whats_New_CSharp_6/Whats_New_CSharp_6/User.cs
+++ Pluralsight/Whats_New_CSharp_6/Whats_New_CSharp_6/User.cs
@@ -1,10 +1,19 @@
 using System;
 
-public static class User
+public class User
 {
+
+    //dont have to write 'set' with this auto prop init syntax
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public string Username { get; protected set; } = String.Empty;
+
+    /** OLD WAY
     public Guid Id
     {
+        // readonly since only a getter
         get { return _id; }
+
     }
 
     public string Username
@@ -13,7 +22,10 @@ public static class User
         protected set { _username = value; }
     }
 
-	// backing fields
-    private readonly Guid _id;
+    // BACKING FIELDS //
+
+    //since readonly, can only be assigned during initialization
+    private readonly Guid _id = Guid.NewGuid();
     private string _username = String.Empty;
+    **/
 }
