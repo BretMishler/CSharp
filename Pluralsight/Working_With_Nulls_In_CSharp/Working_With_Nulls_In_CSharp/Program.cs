5a02f992a73b506beafc94bbb3be6b311a983a46
Bret Mishler <positivecharge8@gmail.com>
2019-02-21 20:39:19 -0800

    Removing empty file; Starting new Pluralsight course

---
+++ Pluralsight/Working_With_Nulls_In_CSharp/Working_With_Nulls_In_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_With_Nulls_In_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
