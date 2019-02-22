c2254978df6989f288a773e2d8712dfcd0398fa3
Bret Mishler <positivecharge8@gmail.com>
2019-02-21 21:00:36 -0800

    Magic Numbers for "nullable" values

---
+++ Pluralsight/Working_With_Nulls_In_CSharp/Working_With_Nulls_In_CSharp/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Working_With_Nulls_In_CSharp
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+        //int and DateTime are structs -> Value type (F12 on them to see)
+        public int DaysSinceLastLogin { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public PlayerCharacter()
+        {
+            DateOfBirth = DateTime.MinValue; // magic number
+            DaysSinceLastLogin = -1; // magic number
+        }
+    }
+}
---
+++ Pluralsight/Working_With_Nulls_In_CSharp/Working_With_Nulls_In_CSharp/PlayerDisplayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Working_With_Nulls_In_CSharp
+{
+    class PlayerDisplayer
+    {
+        /// <summary>
+        /// Output player details
+        /// </summary>
+        /// <param name="player"></param>
+        public static void Write(PlayerCharacter player)
+        {
+            Console.WriteLine(player.Name);
+
+            // DaysSinceLastLogin could be null if they just created an account and have never logged in
+            // so we'll choose an arbitrary value as a "magic number" for nulls
+            if (player.DaysSinceLastLogin == -1)
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            else
+                Console.WriteLine(player.DaysSinceLastLogin);
+
+            // choosing an arbitrary value -- this time a constant
+            if (player.DateOfBirth == DateTime.MinValue)
+                Console.WriteLine("No DateOfBirth specified");
+            else
+                Console.WriteLine(player.DateOfBirth);
+
+
+
+        }
+    }
+}
