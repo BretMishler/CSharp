d73a616afa4004d4a3ede80b93b937b2d02d2f54
Bret Mishler <positivecharge8@gmail.com>
2022-02-08 21:36:31 -0800

    new book

---
+++ CSharpAndTheDotNetFramework/CSharpAndTheDotNetFramework/UnderstandingObjectLifetime/Demo13.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpAndTheDotNetFramework.UnderstandingObjectLifetime
+{
+    public class Demo13
+    {
+        public Demo13()
+        {
+            bool b;
+
+        }
+    }
+}
